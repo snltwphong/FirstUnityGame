db02f0667eb078a88077cd6fd65503104e3ca2cf
NTCPhong <snltwphong@gmail.com>
2023-12-02 10:42:30 +0700

    2/12/2023

---
+++ 1stGame/Assets/Scripts/Lives.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lives : MonoBehaviour
+{
+    public static int totalLive = 3;
+    void Start()
+    {
+          
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- 1stGame/Assets/Scripts/Player.cs
+++ 1stGame/Assets/Scripts/Player.cs
@@ -1,35 +1,89 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     public float speed = 5.0f;
+    public int keys = 0;
+    public TMPro.TextMeshProUGUI LiveText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        LiveText.text = "Lives:" + Lives.totalLive.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
-            transform.Translate(-speed*Time.deltaTime,0,0);
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
         }
-        if(Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            transform.Translate(speed * Time.deltaTime,0,0);
+            transform.Translate(speed * Time.deltaTime, 0, 0);
         }
-        if(Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
-            transform.Translate(0, speed * Time.deltaTime,0);
+            transform.Translate(0, speed * Time.deltaTime, 0);
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
         {
             transform.Translate(0, -speed * Time.deltaTime, 0);
         }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            if (Lives.totalLive >0)
+            {
+                Lives.totalLive--;
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+            else
+            {
+                SceneManager.LoadScene("MenuStartScreen");
+                Lives.totalLive = 3;
+            }
             
+        }
+        if (collision.gameObject.tag == "Door")
+        {
+            switch (SceneManager.GetActiveScene().name)
+            {
+                case "lv1":
+                    SceneManager.LoadScene("lv2");
+                    break;
+                case "lv2":
+                    if (keys == 2)
+                    {
+                        SceneManager.LoadScene("lv3");
+
+                    }
+                    break;
+
+                case "lv3":
+                    if (keys==2)
+                    {
+                        SceneManager.LoadScene("lv4");
+                    }    
+
+                    break;
+
+
+
+            }
+        }
+        if (collision.gameObject.tag == "Key")
+        {
+            keys += 1;
+            Destroy(collision.gameObject);
+        }
     }
 }
